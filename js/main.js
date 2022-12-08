b43f8ce8635f5c9feca115138e03b2eae0fcfac6
John Doe <johndoe@example.com>
2022-12-08 10:28:01 +0400

    added left-nav

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+const leftNav = document.querySelector('.left-nav')
+const hamburger = document.querySelector('nav .menu img')
+
 document.getElementById('search-img').addEventListener('click',()=>{
     document.querySelector('nav .search input').style.width = "200px";
+})
+
+hamburger.addEventListener('click',()=>{
+
+    if (leftNav.classList.contains('d-none')){
+        leftNav.classList.remove('d-none')
+        hamburger.src = "img/close.svg"
+    }else{
+        leftNav.classList.add('d-none')
+        hamburger.src = "img/menu.png"
+    }
+
 })
